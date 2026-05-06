4ad6f0fa3abd5f93d0cbd2b1b87e944fc2824cdc
Mike <mgonzalezobenza@gmail.com>
2026-05-06 12:46:23 +0200

    feat: implementation of PaperBeatsRock

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -9,8 +9,19 @@ public class Tests {
 		Assert.That(RockPaperScissors("Scissors", "Rock"), Is.EqualTo("Player2"));
 	}
 
+	[Test]
+	public void PaperBeatsRock()
+	{
+		Assert.That(RockPaperScissors("Paper", "Rock"), Is.EqualTo("Player1"));
+		Assert.That(RockPaperScissors("Rock", "Paper"), Is.EqualTo("Player2"));
+	}
+
 	private string RockPaperScissors(string player1, string player2) {
+		if (player2 == "Paper")
+			return "Player2";
 		if (player1 == "Rock")
+			return "Player1";
+		if (player1 == "Paper")
 			return "Player1";
 		return "Player2";
 	}
